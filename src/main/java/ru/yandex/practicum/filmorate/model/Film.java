01c68fdec7c78ffac4d7d339476a52e41511b9fb
Polina <nikulschina-p@yandex.ru>
2025-10-29 20:18:08 +0400

    add Constructor for all args

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,10 +2,12 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class Film {
 
     private Integer id;
