8836b1104b26ffd7ce604fa3e9e5de829b000f1a
Polina <nikulschina-p@yandex.ru>
2025-10-29 20:19:10 +0400

    add Tests for validation

---
+++ src/test/java/ru/yandex/practicum/filmorate/validator/ValidatorUserTests.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.validator;
+
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+public class ValidatorUserTests {
+
+    @Test
+    void shouldNotThrowExceptionForEmail() {
+        User user = new User(1, "nik@.com", "login", "name"
+                , LocalDate.of(2004, 5, 21));
+        UserValidator.validate(user);
+    }
+
+    @Test
+    void shouldNotThrowExceptionForNameAndSetName() {
+        User user = new User(1, "nik@.com", "login", ""
+                , LocalDate.of(2004, 5, 21));
+        UserValidator.validate(user);
+        System.out.println("имя пользователя: " + user.getName());
+    }
+
+    @Test
+    void shouldNotExceptionForBirthday() {
+        User user = new User(1, "nik@.com", "login", "name"
+                , LocalDate.now());
+        UserValidator.validate(user);
+    }
+}
