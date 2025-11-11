29621ace332b0123cafc8b09b6e44deab23da980
Polina <nikulschina-p@yandex.ru>
2025-11-11 18:01:17 +0400

    fix all tests

--- src/test/java/ru/yandex/practicum/filmorate/validator/ValidatorFilmTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/validator/ValidatorFilmTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 
 public class ValidatorFilmTests {
 
@@ -13,7 +14,7 @@ public class ValidatorFilmTests {
                 , "\"Дом Кобб – вор, который крадет секреты из подсознания."
                 + " Ему предлагают невыполнимую задачу – внедрить идею в сознание человека."
                 + " Если он преуспеет, это станет идеальным преступлением. Но враги повсюду,"
-                , LocalDate.now(), 100);
+                , LocalDate.now(), 100, new HashSet<>());
         System.out.println("длина описание фильма 1: " + film1.getDescription().length());
         FilmValidator.validate(film1);
 
@@ -21,7 +22,7 @@ public class ValidatorFilmTests {
                 , "Энди Дюфрейн, невинно осужденный за убийство жены, попадает в тюрьму Шоушенк."
                 + " За 20 лет он находит неожиданные способы выжить и сохранить надежду."
                 + " Его дружба с Редом и тихая борьба с системой меняют ж"
-                , LocalDate.now(), 200);
+                , LocalDate.now(), 200, new HashSet<>());
         System.out.println("длина описание фильма 2: " + film2.getDescription().length());
         FilmValidator.validate(film2);
     }
@@ -32,7 +33,7 @@ public class ValidatorFilmTests {
                 , "\"Дом Кобб – вор, который крадет секреты из подсознания."
                 + " Ему предлагают невыполнимую задачу – внедрить идею в сознание человека."
                 + " Если он преуспеет, это станет идеальным преступлением. Но враги повсюду,"
-                , LocalDate.of(1985, 12, 28), 100);
+                , LocalDate.of(1985, 12, 28), 100, new HashSet<>());
         FilmValidator.validate(film1);
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/validator/ValidatorUserTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/validator/ValidatorUserTests.java
@@ -4,20 +4,21 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 
 public class ValidatorUserTests {
 
     @Test
     void shouldNotThrowExceptionForEmail() {
         User user = new User(1, "nik@.com", "login", "name"
-                , LocalDate.of(2004, 5, 21));
+                , LocalDate.of(2004, 5, 21), new HashSet<>());
         UserValidator.validate(user);
     }
 
     @Test
     void shouldNotThrowExceptionForNameAndSetName() {
         User user = new User(1, "nik@.com", "login", ""
-                , LocalDate.of(2004, 5, 21));
+                , LocalDate.of(2004, 5, 21), new HashSet<>());
         UserValidator.validate(user);
         System.out.println("имя пользователя: " + user.getName());
     }
@@ -25,7 +26,7 @@ public class ValidatorUserTests {
     @Test
     void shouldNotExceptionForBirthday() {
         User user = new User(1, "nik@.com", "login", "name"
-                , LocalDate.now());
+                , LocalDate.now(), new HashSet<>());
         UserValidator.validate(user);
     }
 }
