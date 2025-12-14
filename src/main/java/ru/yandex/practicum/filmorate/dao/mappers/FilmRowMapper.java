04b14be2bd7da9ecb039677d3f47664179d23750
Polina <nikulschina-p@yandex.ru>
2025-12-14 11:18:22 +0400

    удалила неиспользуемый импорт

--- src/main/java/ru/yandex/practicum/filmorate/dao/mappers/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/mappers/FilmRowMapper.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.dao.mappers;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Rating;
 
 import java.sql.ResultSet;
