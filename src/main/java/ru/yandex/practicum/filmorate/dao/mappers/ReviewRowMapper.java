4468988fb5442ef86a0113f3e567a76277db0edb
Polina <nikulschina-p@yandex.ru>
2025-12-16 23:35:17 +0400

    добавила маппер для отзывов

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/mappers/ReviewRowMapper.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.dao.mappers;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class ReviewRowMapper implements RowMapper<Review> {
+
+    @Override
+    @SuppressWarnings("null")
+    public Review mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return Review.builder()
+                .id(rs.getLong("review_id"))
+                .content(rs.getString("content"))
+                .positive(rs.getBoolean("is_positive"))
+                .userId(rs.getLong("user_id"))
+                .filmId(rs.getLong("film_id"))
+                .useful(rs.getInt("useful"))
+                .build();
+    }
+}
