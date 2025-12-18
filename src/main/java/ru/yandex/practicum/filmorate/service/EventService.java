c8de0ecb40d87ea348d226faa4dee847a5bcd808
Anton Antonov <vd89@yandex.ru>
2025-12-18 23:46:11 +1100

    fix for postman tests

--- src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.filmorate.dao.repositories.EventDbStorage;
 import ru.yandex.practicum.filmorate.model.Event;
 import ru.yandex.practicum.filmorate.model.enums.EventTypes;
@@ -19,13 +20,15 @@ public class EventService {
         this.eventsDbStorage = eventsDbStorage;
     }
 
+    @Transactional
     public List<Event> getFeed(Long userId) {
-        log.info("Получение ленты событий user ID {}", userId);
+        log.info("EventService: Получение ленты событий user ID {}", userId);
         return List.copyOf(eventsDbStorage.getFeed(userId));
     }
 
+    @Transactional
     public Event addEvent(Long userId, EventTypes event, OperationTypes operation, Long entityId) {
-        log.info("Добавление события {}-{} в ленту user ID {}", event, operation, userId);
+        log.info("EventService: Добавление события {}-{} в ленту user ID {}", event, operation, userId);
         return eventsDbStorage.addEvent(userId, event, operation, entityId);
     }
 }
